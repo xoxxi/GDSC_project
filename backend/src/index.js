0910f7b9451bd416221e925b06d98726fca45312
xoxxi <139968832+xoxxi@users.noreply.github.com>
2025-12-04 15:57:27 +0900

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { solveSudoku, isValidPuzzle } = require('./sudokuSolver');
+const { solveSudoku, isValidPuzzle, generateRandomPuzzle } = require('./sudokuSolver');
 
 const app = express();
 app.use(cors({
