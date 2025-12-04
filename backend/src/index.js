3a37bd905e30430810cfd94abcf16b6479632695
xoxxi <139968832+xoxxi@users.noreply.github.com>
2025-12-04 15:40:33 +0900

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,14 +36,14 @@ app.post('/api/solve', (req, res) => {
 });
 
 
-/* app.get('/api/generate', (req, res) => {
+app.get('/api/generate', (req, res) => {
   try {
     const puzzle = generateRandomPuzzle();
     res.json({ puzzle });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-}); */
+});
 
 const PORT = 3000;
 const HOST = '0.0.0.0'; // 모든 IP 주소에서 접근 가능하게 설정
